cc4579e6d061a3ed2007311eb3b65d9a27a93cd8
jirapornNgewdang <jiraporn7494@gmail.com>
2020-11-16 22:12:41 +0700

    1.15
    Tum Login sed laew

--- LoginViewController.swift
+++ LoginViewController.swift
@@ -6,14 +6,16 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Firebase
 
 class LoginViewController: UIViewController {
 
    
     @IBOutlet weak var ErrorLabel: UILabel!
     @IBOutlet weak var LoginButton: UIButton!
-    @IBOutlet weak var LastNameTextField: UITextField!
-    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,21 +26,36 @@ class LoginViewController: UIViewController {
         // Hide the error label
         ErrorLabel.alpha = 0
         //Style the elements
-        Utilities.styleTextField(firstNameTextField)
-        Utilities.styleTextField(LastNameTextField)
+        Utilities.styleTextField(emailTextField)
+        Utilities.styleTextField(passwordTextField)
         Utilities.styleFilledButton(LoginButton)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-        func loginTapped(_ sender: Any) {
-    }
     
 }
+    
+    @IBAction func loginTapped(_ sender: Any) {
+        // TODO: Validate Text Fields
+        
+        // Create cleaned versions of the text field
+        let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // Signing in the user
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            
+            if error != nil {
+                // Couldn't sign in
+                self.ErrorLabel.text = error!.localizedDescription
+                self.ErrorLabel.alpha = 1
+            }
+            else {
+                
+                let homeViewController = self.storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewController) as? HomeViewController
+                
+                self.view.window?.rootViewController = homeViewController
+                self.view.window?.makeKeyAndVisible()
+            }
+        }
+    }
+    
 }
