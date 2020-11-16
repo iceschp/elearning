a26b7df81a637df270d6967692dfd2cfd5051251
jirapornNgewdang <jiraporn7494@gmail.com>
2020-11-16 15:27:22 +0700

    59 min

--- SignUpViewController.swift
+++ SignUpViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignUpViewController: UIViewController {
 
@@ -33,19 +34,61 @@ class SignUpViewController: UIViewController {
         Utilities.styleFilledButton(SignUpButton)
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func validateFields() -> String? {
+        // Check that all fields are filled in
+        if FirstNametextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            LastNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+        EmailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+           PasswordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            
+            
+        return "Please fill in all fields"
+      }
+        //Check if the password  is secure
+        let cleanedPassword = PasswordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        if Utilities.isPasswordValid(cleanedPassword) == false {
+            //Password isn't secure enough
+            return "Please make sure your password is at least 8 characters , contains a special character and a number."
+        }
+       return nil
     }
-    */
     
     
-    @IBAction func SignUpTepped(_ sender: Any) {
+    
+    
+    
+    @IBAction func SignUpTepped(_ sender: Any)
+    {
+        // Validate the fields
+        let error = validateFields()
+        if error != nil {
+            // There's something wrong with the fields, show error message
+            showErroe(error!)
+        }
+        else {
+            // Create the users
+            Auth.auth().createUser(withEmail: <#T##String#>, password: <#T##String#>) {
+                (result,err) in
+                if err != nil {
+                    // There was an error creating the user
+                    self.showErroe("Error creating user")
+                }
+                else {
+                    // User was created successfully ,nowstore the first name and last name
+                }
+            }
+            // Transsition to the home screen
+        }
+        
     }
-    
-
+    func showErroe(_ message:String) {
+        ErrorLabel.text = message
+        ErrorLabel.alpha = 1
+    }
 }
+
+    
+  
+    
+
+
